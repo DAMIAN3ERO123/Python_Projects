8671adae1cff8d21e69f90fbe354b93b510b446e
DAMIAN3ERO123 <36737516+DAMIAN3ERO123@users.noreply.github.com>
2023-07-25 13:05:15 -0600

    Create band_generator.py

---
+++ band_generator.py
@@ -0,0 +1,6 @@
+# Este proyecto genera un numbre de banda de musica al unir el nombre de la ciudad donde creciste más el nombre de tu mascota.
+print("Welcome to the band name generator")
+city = input("What is the name where you grew up?\n")
+pet = input("What is your pet's name?\n")
+# Contatenación de strings
+print("The name of your band is " + city + " " + pet)
